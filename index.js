a6543d7af2f7e076b82b9747c149f3e81eff811e
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2024-05-10 09:38:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -14,3 +15,27 @@ app.get('/sobre', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+*/
+
+const { Client } = require('pg');
+const parse = require('pg-connection-string').parse;
+
+// URL de conexão fornecido
+const connectionString = 'postgres://postgres.ybwwyderfuvtgpyiymmr:[OIOXjmxOA0nZF6lx]@aws-0-sa-east-1.pooler.supabase.com:5432/postgres';
+
+// Analisar a URL de conexão
+const config = parse(connectionString);
+
+// Criar uma nova instância do cliente PostgreSQL
+const client = new Client(config);
+
+// Conectar ao banco de dados
+client.connect()
+    .then(() => console.log('Conexão com o PostgreSQL estabelecida com sucesso'))
+    .catch(err => console.error('Erro ao conectar com o PostgreSQL', err));
+
+// Execute consultas SQL, por exemplo:
+client.query('SELECT * FROM clientes')
+    .then(res => console.log(res.rows))
+    .catch(err => console.error('Erro ao executar consulta', err))
+    .finally(() => client.end()); // Feche a conexão quando não estiver em uso
