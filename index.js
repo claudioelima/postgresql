17df09f9d7f71d1408e711b03c2f4dc88418a90b
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2024-05-10 10:40:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
-/*
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Bem-vindo à página inicial!');
-  });
-
-app.get('/sobre', (req, res) => {
-    res.send('Esta é a página de informações sobre o nosso aplicativo.');
-  });
-    
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+// Configuração da conexão com o banco de dados PostgreSQL
+const pool = new Pool({
+    user: 'postgres.ybwwyderfuvtgpyiymmr',
+    host: 'aws-0-sa-east-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'OIOXjmxOA0nZF6lx',
+    port: 5432,
 });
-*/
 
-const { Client } = require('pg');
-const parse = require('pg-connection-string').parse;
+// Rota para acessar os dados do banco de dados
+app.get('/usuarios', async (req, res) => {
+    try {
+        // Consulta SQL para selecionar todos os usuários da tabela 'usuarios'
+        const { rows } = await pool.query('SELECT * FROM clientes');
+        res.json(rows); // Retorna os dados como JSON
+    } catch (err) {
+        console.error('Erro ao consultar dados do banco de dados:', err);
+        res.status(500).json({ error: 'Erro ao consultar dados do banco de dados' });
+    }
+});
 
-// URL de conexão fornecido
-const connectionString = 'postgres://postgres.ybwwyderfuvtgpyiymmr:[OIOXjmxOA0nZF6lx]@aws-0-sa-east-1.pooler.supabase.com:5432/postgres';
-
-// Analisar a URL de conexão
-const config = parse(connectionString);
-
-// Criar uma nova instância do cliente PostgreSQL
-const client = new Client(config);
-
-// Conectar ao banco de dados
-client.connect()
-    .then(() => console.log('Conexão com o PostgreSQL estabelecida com sucesso'))
-    .catch(err => console.error('Erro ao conectar com o PostgreSQL', err));
-
-// Execute consultas SQL, por exemplo:
-client.query('SELECT * FROM clientes')
-    .then(res => console.log(res.rows))
-    .catch(err => console.error('Erro ao executar consulta', err))
-    .finally(() => client.end()); // Feche a conexão quando não estiver em uso
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
