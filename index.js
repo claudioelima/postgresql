bb4fdd5ca2823ae598ec2a6dd945026b1e1dc7ac
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2024-05-22 11:39:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-
 // Inicializando o aplicativo Express
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,19 +11,27 @@ const port = process.env.PORT || 3000;
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Supabase URL e KEY são necessários.');
+  process.exit(1);
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.get('/', async (req, res) => {
   try {
-      // Consulta SQL para selecionar todos os usuários da tabela 'clientes' usando Supabase
-      const { data, error } = await supabase.from('clientes').select('*');
-      if (error) {
-          throw error;
-      }
-      res.json(data); // Retorna os dados como JSON
+    console.log('Iniciando consulta ao banco de dados...');
+    // Consulta SQL para selecionar todos os usuários da tabela 'clientes' usando Supabase
+    const { data, error } = await supabase.from('clientes').select('*');
+    if (error) {
+      console.error('Erro retornado pela consulta:', error);
+      throw error;
+    }
+    console.log('Dados consultados com sucesso:', data);
+    res.json(data); // Retorna os dados como JSON
   } catch (err) {
-      console.error('Erro ao consultar dados do banco de dados:', err.message);
-      res.status(500).json({ error: 'Erro ao consultar dados do banco de dados' });
+    console.error('Erro ao consultar dados do banco de dados:', err);
+    res.status(500).json({ error: 'Erro ao consultar dados do banco de dados', detalhes: err.message });
   }
 });
 
