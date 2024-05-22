b79db3a519e9d504004c4fd892062b373739529d
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2024-05-22 11:06:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = 3000;
 
 // Configurações do Supabase
 const supabaseUrl = 'https://ybwwyderfuvtgpyiymmr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqZHpleW5ycXRqb2lrcm1idHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNjA3ODYsImV4cCI6MjAyNzgzNjc4Nn0.F61eIfrB3hiGVa6iZ0qs4guzXBJvesineM5cCVAkcuo';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlid3d5ZGVyZnV2dGdweWl5bW1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzMzcxMzUsImV4cCI6MjAzMDkxMzEzNX0.4onQPDF-kGwvuUE1WvFiD41Dv7hgvLob0V6b3fh88iw';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Rota para acessar os dados do banco de dados
